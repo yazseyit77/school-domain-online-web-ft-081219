ded49d44c1b327273d75ea787723466cd850c675
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-21 13:45:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,11 @@
 # code here!
 class School
-  def initialize()
+  def initialize(name)
+    @name = "Bayside High School"
+  end
 
+  roster = {}
+  def roster=(roster)
+    @roster = roster
   end
 end
