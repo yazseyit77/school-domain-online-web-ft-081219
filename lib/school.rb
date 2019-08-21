731a95f0927a70a457337c9f7d31f0339e391e1d
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-21 13:51:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,7 @@
 # code here!
 class School
   attr_reader :name, :breed
-  def initialize(name  = "Bayside High School", new)
+  def initialize(name  = "Bayside High School", new = {})
     @name = name
     @new = new
   end
