8e57564a246f0e530baa85ce2a94b90f488a5f53
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-21 13:49:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,9 @@
 # code here!
 class School
   attr_reader
-  def initialize(name, roster)
-    @name = "Bayside High School"
-    @roster = {}
+  def initialize(name  = "Bayside High School", roster = {})
+    @name = name
+    @roster = roster
   end
 
 
